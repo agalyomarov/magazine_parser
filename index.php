220afa157f9d9f2e23e88415e39cee69b736100f
Agaly Omarov <97389793+agalyomarov@users.noreply.github.com>
2022-08-13 16:10:08 +0300

    yes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 ini_set('log_errors', 'On');
 ini_set('error_log', 'php_errors.log');
-ini_set("max_execution_time", "7200");
+ini_set("max_execution_time", "10000");
 ini_set('allow_url_fopen', 1);
 ini_set('memory_limit', '10000M');
 
@@ -33,33 +33,43 @@ $writer->save('p.xlsx');
 $site = "https://www.planeta-sirius.ru";
 $catalogs_content = file_get_contents("https://www.planeta-sirius.ru/katalog/");
 $dom = new Dom;
-$dom2 = new Dom;
 $dom->loadStr($catalogs_content);
 $catalogs_lists = $dom->find('#catalog_category .subsection')[0];
 $catalogs_href = [];
+unset($dom);
 foreach ($catalogs_lists as $a) {
+   $dom2 = new Dom;
    if ($a->getAttribute('href')) {
       $key = $site . $a->getAttribute('href');
+      unset($a);
       $catalogs_href[$key] = [];
       $dom2->loadStr(file_get_contents($key));
       $pod_catalogs_lists = $dom2->find('#catalog_category .subsection')[0];
+      unset($dom2);
       if ($pod_catalogs_lists) {
          foreach ($pod_catalogs_lists as $link) {
             if ($link->getAttribute('href')) {
                $catalogs_href[$key][] = $site . $link->getAttribute('href');
+               unset($link);
             }
          }
       }
    }
+   unset($dom2);
 }
 $count = 2;
 foreach ($catalogs_href as $key => $href) {
+   $dom = new Dom;
    if (count($href) > 0) {
       foreach ($href as $link) {
          $content = file_get_contents($link);
+         unset($link);
          $dom->loadStr($content);
          $cards = $dom->find('#catalog_category .row')[1];
+         unset($dom);
+         unset($content);
          foreach ($cards as $card) {
+            $dom = new Dom;
             $article = $card->find('.artikul')[0];
             $article = strip_tags($article);
             $a = $card->find('h4 a')[0];
@@ -72,16 +82,20 @@ foreach ($catalogs_href as $key => $href) {
             if ($a) {
                $href = $a->getAttribute('href');
                $product = file_get_contents($site . $href);
-               $dom2->loadStr($product);
-               $desc = $dom2->find('.product_description')[0];
+               $dom->loadStr($product);
+               unset($product);
+               $desc = $dom->find('.product_description')[0];
+               unset($dom);
                $desc = $desc->text;
                $title = $a->text;
+               unset($a);
                $price_opt = str_replace('Цена', '', strip_tags($price_opt));
                $price_opt = str_replace("&#8381;", '₽', strip_tags($price_opt));
                $price_rozn = str_replace('Цена', '', strip_tags($price_rozn));
                $price_rozn = str_replace("&#8381;", '₽', strip_tags($price_rozn));
                $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
                $spreadsheet = $reader->load("p.xlsx");
+               unset($reader);
                $sheet = $spreadsheet->getActiveSheet();
                $sheet->setCellValue('A' . $count, $article);
                $sheet->setCellValue('B' . $count, htmlspecialchars_decode($title));
@@ -92,13 +106,20 @@ foreach ($catalogs_href as $key => $href) {
                $writer = new Xlsx($spreadsheet);
                $writer->save('p.xlsx');
                $count++;
+               // if ($count > 10) die;
                echo $count . "<br>";
+               unset($sheet);
+               unset($writer);
+               unset($spreadsheet);
             }
+            unset($card);
          }
       }
    } else {
+      $dom = new Dom;
       $content = file_get_contents($key);
       $dom->loadStr($content);
+      unset($content);
       $cards = $dom->find('#catalog_category .row')[1];
       foreach ($cards as $card) {
          $article = $card->find('.artikul')[0];
@@ -113,8 +134,8 @@ foreach ($catalogs_href as $key => $href) {
          if ($a) {
             $href = $a->getAttribute('href');
             $product = file_get_contents($site . $href);
-            $dom2->loadStr($product);
-            $desc = $dom2->find('.product_description')[0];
+            $dom->loadStr($product);
+            $desc = $dom->find('.product_description')[0];
             $desc = $desc->text;
             $title = $a->text;
             $price_opt = str_replace('Цена', '', strip_tags($price_opt));
@@ -123,6 +144,7 @@ foreach ($catalogs_href as $key => $href) {
             $price_rozn = str_replace("&#8381;", '₽', strip_tags($price_rozn));
             $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
             $spreadsheet = $reader->load("p.xlsx");
+            unset($reader);
             $sheet = $spreadsheet->getActiveSheet();
             $sheet->setCellValue('A' . $count, $article);
             $sheet->setCellValue('B' . $count, htmlspecialchars_decode($title));
@@ -133,9 +155,16 @@ foreach ($catalogs_href as $key => $href) {
             $writer = new Xlsx($spreadsheet);
             $writer->save('p.xlsx');
             $count++;
+            // if ($count > 10) die;
             echo $count . "<br>";
+            unset($writer);
+            unset($spreadsheet);
+            unset($sheet);
          }
+
+         unset($card);
       }
+      unset($dom);
    }
 }
 
